141f3c7865958fbfd492f3aba358dbc08f34f53f
Arturo <a.gonzalezii609@gmail.com>
2013-04-03 22:32:27 -0400

    Minor Changes

--- src/com/example/interq/MainActivity.java
+++ src/com/example/interq/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.interq;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Display;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
